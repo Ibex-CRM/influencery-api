9636d7d0f0e4fdd1ff1f7be09b78f5779d0eafa4
Carly <carly.jenkinson65@googlemail.com>
2022-04-27 11:19:35 +0200

    Add database config. Add additional validation in influncer model.

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -1,6 +1,6 @@
 class Influencer < ApplicationRecord
   belongs_to :platform
-  belongs_to :primary_tag, class_name: "Tag", foreign_key: :id
+  belongs_to :primary_tag, class_name: "Tag", foreign_key: :id, optional: true
   has_many :influencer_tags
   has_many :tags, through: :influencer_tags
 
